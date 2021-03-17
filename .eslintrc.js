7937fea85c2dbb98a0068cc07e2afe94185a4237
idovzhikov <ivan.dovzhikov@softgamings.com>
2021-03-17 22:36:22 +0300

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    root: true,
+    overrides: [
+        {
+            files: ['*.ts'],
+            parserOptions: {
+                project: [
+                    'tsconfig.*?.json',
+                    'e2e/tsconfig.json',
+                ],
+                createDefaultProgram: true,
+            },
+            extends: [
+                'plugin:@angular-eslint/recommended',
+                'airbnb-typescript/base',
+            ],
+            rules: {
+                'import/prefer-default-export': 'off',
+                'arrow-parens': 'off',
+                'no-console': 'off',
+                'indent': ['error', 4],
+                '@typescript-eslint/indent': ['error', 4],
+            },
+        },
+        {
+            files: ['*.component.html'],
+            extends: ['plugin:@angular-eslint/template/recommended'],
+            rules: {
+                'max-len': ['error', { 'code': 140 }],
+            },
+        },
+        {
+            files: ['*.component.ts'],
+            extends: ['plugin:@angular-eslint/template/process-inline-templates'],
+        },
+    ],
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DesignModule } from 'src/design/design.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        AppRoutingModule,
+        DesignModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
