a37f4d2f4b3e75a12a0d886814b1a3e0fa9b4aae
Ivan Dovzhikov <einegesit@gmail.com>
2021-03-28 21:40:53 +0300

    Add design root directive

---
+++ src/app/design/root/design-root.directive.ts
@@ -0,0 +1,49 @@
+import {
+    Directive,
+    HostBinding,
+    Inject,
+    OnDestroy,
+    SecurityContext,
+} from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
+
+import { Design, DESIGN } from './design.provider';
+
+type CssVariables = { [variable: string]: string };
+
+@Directive({
+    selector: '[appDesignRoot]',
+})
+export class DesignRootDirective implements OnDestroy {
+    @HostBinding('style')
+    public rootVariables!: SafeStyle;
+
+    private subscription: Subscription;
+
+    constructor(
+    @Inject(DESIGN)
+        design: Design,
+        sanitizer: DomSanitizer,
+    ) {
+        this.subscription = design.currentTheme.subscribe(theme => {
+            const { colors, surfaces } = design.themes[theme];
+            const cssVariables: CssVariables = {};
+
+            Object.entries(colors).forEach(([name, value]) => {
+                cssVariables[`--color-${name}`] = value;
+            });
+
+            Object.entries(surfaces).forEach(([level, { color, shadow }]) => {
+                cssVariables[`--surface${level}-color`] = color;
+                cssVariables[`--surface${level}-shadow`] = shadow;
+            });
+
+            this.rootVariables = sanitizer.sanitize(SecurityContext.STYLE, cssVariables) as string;
+        });
+    }
+
+    public ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+}
