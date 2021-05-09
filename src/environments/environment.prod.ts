89dae4d0b2f0bc58f37014ff3466c31fdee47e3f
Ivan Dovzhikov <einegesit@gmail.com>
2021-05-09 16:26:59 +0300

    Add firebase configuration for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
     production: true,
+    firebase: {
+        apiKey: 'AIzaSyAAfHYSyOk-gMmSaiNrpzG0JgChQ4jzAdI',
+        authDomain: 'r-chat-prod.firebaseapp.com',
+        projectId: 'r-chat-prod',
+        storageBucket: 'r-chat-prod.appspot.com',
+        messagingSenderId: '543840815164',
+        appId: '1:543840815164:web:3b2a286683b878122a1b39',
+    },
 };
